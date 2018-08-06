eed0ec090c0464af7b5508c78853ceb2a9f79969
PolarisTLX <paul.a.rail@gmail.com>
2018-08-06 15:37:07 -0400

    completed todo-list app
    Co-authored-by: Kyle Lemon <james.kyle.lemon@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+// import './index.css';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
