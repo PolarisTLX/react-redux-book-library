f363c8c58e993ed6108667bdb89cbce9a13933e4
PolarisTLX <paul.a.rail@gmail.com>
2018-08-14 14:01:07 -0400

    modifications to allow Netlify deployment
    Co-authored-by: Kyle Lemon <james.kyle.lemon@gmail.com>

--- src/store.js
+++ src/store.js
@@ -8,7 +8,7 @@ const middleware = [thunk];
 
 const store = createStore(rootReducer, initialState, compose(
   applyMiddleware(...middleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
 
 
